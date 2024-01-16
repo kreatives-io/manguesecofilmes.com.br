c44c9bc05c3bc51a5f1095aeef7ddeac461c0b3e
Caio Alcantara <alcantaracaiolucas@gmail.com>
2024-01-16 11:15:10 -0800

    Feat/add hero hover (#6)
    * feat: add hero hover animation
    
    * style: add transition
    
    * feat: add hero navigation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,10 @@
 import { Hero } from "@/components";
 import { client } from "@/lib/sanity/lib/client";
 
-export default async function Home() {
+export default async function Home({ params: { locale } }) {
   const works = await client.fetch(
     `*[_type == "work"] { category, artist, slug, publishedAt, mainImage, workName }`
   );
 
-  return <Hero works={works} />;
+  return <Hero works={works} locale={locale} />;
 }
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+'use client';
+
 import Image from "next/image";
-import { useNextSanityImage } from 'next-sanity-image';
+import Link from "next/link";
+
 import useImageGenerator from "@/hooks/useImageGenerator";
 
 interface IHero {
   works: any;
+  locale: string;
 }
 
 const GRID_IMG_SIZE = 9;
 
-export default function Hero({ works }: IHero) {
+export default function Hero({ works, locale }: IHero) {
   const gridArray = works.slice(0, GRID_IMG_SIZE);
   const generateImage = (imageData: any) => useImageGenerator(imageData);
 
@@ -18,16 +21,22 @@ export default function Hero({ works }: IHero) {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {gridArray.map((item: any, index: number) => {
           const { width, height, src } = generateImage(item.mainImage) as any;
-
+          const workLink = `/${locale}/works/${works[index].slug.current}`;
+          
           return (
-            <Image
-              key={index}
-              src={src}
-              alt="hero image"
-              width={width}
-              height={height}
-              className="object-cover h-full w-full"
-            />
+            <Link key={index} className="opacity-100 relative cursor-pointer" href={workLink}>
+              <div className="absolute inset-0 bg-black opacity-0 hover:opacity-70 flex flex-col items-center gap-2 justify-center ease-in-out transition-opacity duration-250">
+                <span className="font-freight text-4xl uppercase">{item.artist}</span>
+                <span className="font-freight text-4xl capitalize">{item.workName}</span>
+              </div>
+              <Image
+                src={src}
+                alt="hero image"
+                width={width}
+                height={height}
+                className="object-cover h-full w-full"
+              />
+            </Link>
           )
         })}
       </div>
