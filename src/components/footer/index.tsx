898ae791734ab47e472505c7daa9e566d1bad0bf
Caio Alcantara <alcantaracaiolucas@gmail.com>
2024-01-16 08:51:36 -0800

    style: footer owner copyright

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="flex w-full h-[100px] items-center justify-center bg-black py-10">
       <p className="hidden lg:flex lg:flex-row gap-0 lg:gap-2">
-        © 2023 edvaldoraw.com - All rights reserved | Built with ❤️ by
+        © 2023 <span className="font-freight text-persian uppercase">edvaldoraw.com</span> - All rights reserved | Built with ❤️ by
         <Link
           href="https://kreatives.io"
           target="_blank"
@@ -14,7 +14,7 @@ export default function Footer() {
         </Link>
       </p>
       <p className="flex flex-col items-center gap-2 lg:hidden">
-        <span>© 2023 <span className="font-freight">edvaldoraw.com</span> - All rights reserved</span>
+        <span>© 2023 <span className="font-freight text-persian uppercase">edvaldoraw.com</span> - All rights reserved</span>
         <span>
           Built with ❤️ by
           <Link
