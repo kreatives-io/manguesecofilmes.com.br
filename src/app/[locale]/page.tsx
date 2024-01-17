6e8daf634e0db5234ab5f6c778ec1a4fbf2b31ad
Caio Alcantara <alcantaracaiolucas@gmail.com>
2024-01-17 11:25:48 -0800

    fix: ts lint

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "@/components";
 import { client } from "@/lib/sanity/lib/client";
 
-export default async function Home({ params: { locale } }) {
+export default async function Home({ params: { locale } }: { params: { locale: string } }) {
   const works = await client.fetch(
     `*[_type == "work"] { category, artist, slug, publishedAt, mainImage, workName }`
   );
